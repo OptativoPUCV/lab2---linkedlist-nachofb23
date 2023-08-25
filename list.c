1b3906da9ef5897ca1bcb6e23d3de3340317fe06
Replit user <>
2023-08-25 19:59:26 +0000

    new try

--- list.c
+++ list.c
@@ -101,8 +101,7 @@ void pushFront(List * list, void * data) {
 }
 
 void pushBack(List * list, void * data) {
-    list->current = list->tail;
-    pushCurrent(list,data);
+    
 }
 
 void pushCurrent(List * list, void * data) {
